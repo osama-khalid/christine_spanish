c08bd2c73ab5b543b89ad25b854748ea1ee7acf4
osama-khalid <khalyd.osama@gmail.com>
2023-03-31 11:40:01 -0500

    Add files via upload

--- spanish_freq.py
+++ spanish_freq.py
@@ -1,12 +1,12 @@
 import os
 import re
+import tqdm
 
 ROOT = './corpus/'
 files = os.listdir(ROOT)
-import spacy
-nlp = spacy.load('es_core_news_md')
-
-for f in files:
+bigram_count = {}
+unigram_count = {}
+for f in tqdm.tqdm(files):
     if f.startswith('spanish'):
         file = open(ROOT+f,'r',encoding = 'latin-1').read()
         docs = file.split('<doc id=')[1:]
@@ -14,6 +14,34 @@ for f in files:
             start = item.find('>')  
             text = item[start+1:].split('</doc>')[0]
             text = text.replace('\n',' ')
-            text = re.sub(' +',' ',text).strip(' ')
-            text_ = nlp(text)
-        print(XXX)
+            text = re.sub(r'[^\w\s]', ' ',text).lower()
+
+            text = [re.sub(' +',' ',text).strip(' ')]
+            unigrams = text[0].split(' ')            
+            bigrams = ['_'.join(b) for l in text for b in zip(l.split(" ")[:-1], l.split(" ")[1:])]
+            uni = list(set(unigrams))
+            for u in uni:
+                if u not in unigram_count:
+                    unigram_count[u]=0
+                unigram_count[u] += unigrams.count(u)
+                
+                
+            bi = list(set(bigrams))
+            for u in bi:
+                if u not in bigram_count:
+                    bigram_count[u]=0
+                bigram_count[u] += bigrams.count(u)                
+                
+                
+import csv
+
+with open('unigram_freq.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',')
+    for u in unigram_count:
+        employee_writer.writerow([u,unigram_count[u]])
+        
+
+with open('biigram_freq.csv', mode='w') as employee_file:
+    employee_writer = csv.writer(employee_file, delimiter=',')
+    for u in unigram_count:
+        employee_writer.writerow([u.replace('_',' '),bigram_count[u]])        
